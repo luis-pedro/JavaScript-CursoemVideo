2f045bc8524f10f8aecd5deaff7f348d17ba892a
Luis Pedro <101891870+luis-pedro@users.noreply.github.com>
2023-02-06 23:17:46 -0300

    Módulo D - aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 60
+
+console.log('A sua velocidade foi de ${vel}Km/h')
+
+if(vel > 60){ // Condição simples
+    console.log('A sua velocidade ultrapassou o limite')
+    console.log('MULTADO')
+}
+
+console.log('Dirija sempre usando o cinto de segurança')
