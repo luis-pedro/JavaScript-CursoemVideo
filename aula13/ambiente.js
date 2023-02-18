6594c747a1adced87ba834494106c880d21e8a64
Luis Pedro <101891870+luis-pedro@users.noreply.github.com>
2023-02-18 11:34:47 -0300

    Aula 13 - Módulo E

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,9 +1,17 @@
 var c = 1
 
+do {
+    console.log('Tudo bem?') /* Teste lógico no final */
+    c++ 
+} while (c <= 6)
+
+
+/*  
 while (c < 6) {
-    console.log('Tudo bem?')
+    console.log('Tudo bem?')    Teste lógico no início 
     c++ 
 }
+*/
 
 /*
 console.log('Tudo bem?')
